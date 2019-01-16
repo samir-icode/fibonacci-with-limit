fc52c21b29e2f525d18015e95fdd7881e71da80e
samir-icode <samir_icode@hotmail.com>
2019-01-16 02:15:38 +0100

    Create fibo.py

---
+++ fibo.py
@@ -0,0 +1,20 @@
+lst = [1,2,3,4,5]    
+indx = first_and_last(lst)
+
+n = int(input("Set fibonaci: "))
+lnt = int(input("Set limit for number to generate: "))
+
+def fibo(n):
+    a,b =0,1
+    res=[]
+    while a<n:
+        res.append(a)
+#        print(a)
+
+        a,b = b,a+b
+#    print(res) print full list
+    for lmt in res[0:lnt]:
+        print(lmt)
+          
+
+fib = fibo(n)
